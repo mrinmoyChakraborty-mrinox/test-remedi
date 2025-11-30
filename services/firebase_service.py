02529992dd5a418968f01dd1ed8cbaa71ca20e87
mrinmoyChakraborty-mrinox <chakrabortymrinmoy27011@gmail.com>
2025-11-30 12:48:44 +0530

    refactor: Remove dotenv loading from firebase_service.py

--- services/firebase_service.py
+++ services/firebase_service.py
@@ -1,8 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, auth, storage
 import os,json
-from dotenv import load_dotenv
-load_dotenv()
 firebase_creds = os.getenv("FIREBASE_CONFIG")
 
 if firebase_creds:
